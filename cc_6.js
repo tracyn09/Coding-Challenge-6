f3086c7a291015f2c1f526bfa59c65cbb3613134
tracyn09 <tracyn0099@gmail.com>
2025-02-04 14:29:17 -0500

    Task 2– Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -4,3 +4,10 @@ const  calculateProfit= function(costPrice, sellingPrice, unitsSold) {
 }
 let totalProfit= calculateProfit(50,70,200)
 console.log(`Total Profit: $${totalProfit}`);
+
+//Task 2
+const calculateSalesTax= function (amount,taxRate) {
+    return amount * taxRate
+}
+let salesTax= calculateSalesTax(500, 0.1)
+console.log(`Sales Tax: $${salesTax}`);
